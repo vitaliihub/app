7be4bd450ce48fdd958fe13deb5b8368783f4190
Vitalii <vitaliimail7@gmail.com>
2023-07-13 10:30:50 +0300

    modified settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -177,10 +177,9 @@ REDIS_DB = env('REDIS_DB')
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": f"redis:{REDIS_HOST}:{REDIS_PORT}/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
-
